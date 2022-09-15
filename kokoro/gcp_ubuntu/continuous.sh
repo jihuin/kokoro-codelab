1676b7bc29b3133a40f9912adb899827316f27c2
Jihui Nie <jihuinie@google.com>
2022-09-15 16:25:20 -0700

    check installed jdks

--- kokoro/gcp_ubuntu/continuous.sh
+++ kokoro/gcp_ubuntu/continuous.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "JAVA_HOME=$JAVA_HOME"
+ls /usr/lib/jvm/*
 #sudo update-java-alternatives --set java-1.11.0-openjdk-amd64
 export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
 echo "JAVA_HOME=$JAVA_HOME"
