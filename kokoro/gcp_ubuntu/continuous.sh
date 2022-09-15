20ad343936d97fff0b84cac52e6f5f2301cf57b4
Jihui Nie <jihuinie@google.com>
2022-09-15 16:22:05 -0700

    update JAVA_HOME

--- kokoro/gcp_ubuntu/continuous.sh
+++ kokoro/gcp_ubuntu/continuous.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+echo "JAVA_HOME=$JAVA_HOME"
+#sudo update-java-alternatives --set java-1.11.0-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+echo "JAVA_HOME=$JAVA_HOME"
 java --version
-echo "JAVA_HOME=$JAVA_HOME"
-sudo update-java-alternatives --set java-1.11.0-openjdk-amd64
-echo "JAVA_HOME=$JAVA_HOME"
