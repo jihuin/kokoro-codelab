f6aad90008ccf234efc8d730f3cf204df8c8679f
Jihui Nie <jihuinie@google.com>
2022-09-15 22:45:27 -0700

    show gpg2 version

--- kokoro/gcp_ubuntu/continuous.sh
+++ kokoro/gcp_ubuntu/continuous.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-echo "JAVA_HOME=$JAVA_HOME"
-ls /usr/lib/jvm/*
-#sudo update-java-alternatives --set java-1.11.0-openjdk-amd64
+# ls /usr/lib/jvm/*
 export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
 echo "JAVA_HOME=$JAVA_HOME"
-java --version
 
 which gpg gpg2
 gpg --version
+gpg2 --version
 gpg -k
